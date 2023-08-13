d4fdc21e12d12e99aad7147f3b38d30fb13d1748
Eugene Cross <eugene.cross@gmail.com>
2023-08-13 22:22:05 +0100

    Initial Commit

---
+++ RockScissorsPaper/RockScissorsPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockScissorsPaperApp.swift
+//  RockScissorsPaper
+//
+//  Created by Eugene on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct RockScissorsPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
