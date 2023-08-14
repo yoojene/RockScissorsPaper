23414ef632d86747c2a1c8cdac4a16da28216b70
Eugene Cross <eugene.cross@gmail.com>
2023-08-14 11:47:11 +0100

    First attempt for game

--- RockScissorsPaper/ContentView.swift
+++ RockScissorsPaper/ContentView.swift
@@ -7,20 +7,248 @@
 
 import SwiftUI
 
+//
+//struct ButtonStyle: ViewModifier {
+//    func body(content: Content) -> some View {
+//        content
+//
+//            .overlay(RoundedRectangle(cornerSize: CGSize(width: 25, height: 25))
+//                                    .stroke(Color.black, lineWidth: 2.5))
+//    }
+//}
 struct ContentView: View {
+
+    let movesDict = [
+        "Rock": "🪨",
+        "Scissors": "✂️",
+        "Paper": "📄"
+    ]
+
+    let moves = [
+        "🪨",
+        "✂️",
+        "📄"
+    ]
+
+    @State private var appChoice = Int.random(in: 0...2)
+    @State private var shouldWin: Bool = Bool.random()
+
+    @State private var score: Int = 0
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+
+    @State private var gameEnded = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        ZStack {
+            LinearGradient(
+                 gradient: Gradient(colors: [Color(red: 0.4, green: 0.5, blue: 0.99), Color.white]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+
+                Text("App has chosen:")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .padding(.bottom)
+
+                Text("\(moves[appChoice])")
+                    .font(.system(size: 50))
+
+                Spacer()
+
+                if shouldWin {
+                    Text("Choose option to win")
+                        .font(.title)
+                        .fontWeight(.bold)
+                } else {
+                    Text("Choose option to lose")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+
+                Spacer()
+
+                VStack(spacing: 15) {
+
+                    // Dictionary way, cannot index by number
+    //                ForEach(moves.sorted(by: >), id: \.key) {key, value in
+    //                    Button {
+    //                        print("tapped button")
+    //                    } label : {
+    //                        Text("\(key) \(value) ")
+    //                            .font(.largeTitle)
+    //                    }
+    //
+    //                }
+
+                    ForEach(0..<moves.count, id: \.self) { number in
+
+                        Button {
+                            calculateWinOrLose(for: number, toWin: shouldWin)
+                           } label : {
+                               Text(moves[number])
+                               .font(.system(size: 100))
+                           }
+                        
+                        }
+                    }
+
+                Spacer()
+
+                Text("Score is \(score)")
+                    .font(.title)
+                Spacer()
+            }
+            .alert("Game Over", isPresented: $gameEnded) {
+                Button("Reset the game") {
+                    resetGame()
+                }
+            } message: {
+                Text("You have \(score) points in total")
+            }
+
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: next)
+        } message:  {
+            Text("Your score is \(score)")
+        }
+
+
+    }
+
+    func calculateWinOrLose(for number: Int, toWin playerToWin: Bool) {
+        print("Player chose \(moves[number]) with \(number)")
+        print("App chose \(moves[appChoice]) with \(appChoice)")
+        print("Player wants to win? \(playerToWin)")
+
+
+        print(moves)
+
+
+        let playerChoice = moves[number]
+        let appChoice = moves[appChoice]
+
+        if appChoice == playerChoice {
+            // neither win nor lose
+            return
+        }
+
+
+
+
+
+
+
+        if appChoice == "✂️" && playerChoice == "🪨" {
+
+            if playerToWin == true {
+                // player wins
+                score += 1
+                scoreTitle = "You Win"
+            } else {
+                score -= 1
+                scoreTitle = "You Lose"
+
+            }
+        }
+        if appChoice == "✂️" && playerChoice == "📄" {
+            // app wins
+            if playerToWin == false {
+                // player wins
+                score += 1
+                scoreTitle = "You Win"
+
+            } else {
+                score -= 1
+                scoreTitle = "You Lose"
+
+            }
+        }
+
+        if appChoice == "🪨" && playerChoice == "✂️"  {
+            // app wins
+
+            if playerToWin == false {
+                // player wins
+                score += 1
+                scoreTitle = "You Win"
+
+            } else {
+                scoreTitle = "You lose"
+                score -= 1
+            }
+        }
+        if appChoice == "🪨" && playerChoice == "📄"  {
+            // player wins
+            if playerToWin == true {
+                // player wins
+                score += 1
+                scoreTitle = "You Win"
+
+            } else {
+                score -= 1
+                scoreTitle = "You lose"
+
+            }
+        }
+
+        if appChoice == "📄" && playerChoice == "✂️" {
+            // player wins
+            if playerToWin == true {
+                // player wins
+                score += 1
+                scoreTitle = "You Win"
+
+            } else {
+                scoreTitle = "You Lose"
+                score -= 1
+            }
+        }
+        if appChoice == "📄" && playerChoice == "🪨" {
+            // app wins
+            if playerToWin == false {
+                // player wins
+                score += 1
+                scoreTitle = "You Win"
+
+            } else {
+                score -= 1
+                scoreTitle = "You Lose"
+
+            }
+        }
+
+        if score > 2 {
+            gameEnded = true
+        } else {
+            showingScore = true
+
+        }
+
+
+
+    }
+
+    func next() {
+
+        appChoice = Int.random(in: 0...2)
+        shouldWin.toggle()
+    }
+
+    func resetGame() {
+        score = 0
+        next()
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
